46d3c0104a2a48239b284ceabfb22474b146592d
Michal Kowalczyk <mail@kowalczykmichal.com>
2020-04-10 17:41:30 +0200

    Add script.js

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var computerMove;
+computerMove = 'rock';
+printMessage('My move is ' + computerMove + '! If your move is paper you win');
+
+var playerMove;
+playerMove = 'paper';
+printMessage('My move is ' + playerMove + 'I won!');
